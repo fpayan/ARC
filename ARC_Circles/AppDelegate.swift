d36f4492601a08a9ec3b7f7a4409d5f356773c3b
Joaquin Perez Barroso <jperbar@me.com>
2018-02-04 22:01:31 +0100

    Ready to show

--- ARC_Circles/AppDelegate.swift
+++ ARC_Circles/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ ARC_Circles/FormTableViewController.swift
@@ -0,0 +1,81 @@
+//
+//  FormTableViewController.swift
+//  ARC_Circles
+//
+//  Created by Joaquin Perez on 04/02/2018.
+//  Copyright © 2018 Joaquin Perez. All rights reserved.
+//
+
+import UIKit
+
+class FormTableViewController: UITableViewController, SelectionTableViewControllerDelegate {
+    
+    lazy var selectionTVC = SelectionTableViewController.init(style: .plain)
+    
+    var option = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
+        tableView.register(UINib.init(nibName: "FormCell", bundle: nil), forCellReuseIdentifier: "formCellReuse")
+        
+    }
+
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "formCellReuse", for: indexPath) as! FormCell
+
+        cell.label.text = "Country"
+        cell.textField.isUserInteractionEnabled = false
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectionTVC.listOfOptions = ["España", "Portugal"]
+        switch option {
+        case 0:
+   
+        selectionTVC.listOfOptions = ["España", "Portugal"]
+        selectionTVC.delegate = self
+        selectionTVC.row = indexPath.row
+            
+        default:
+            selectionTVC.selectionClosure = { // [unowned self]
+                string in
+                let cell = self.tableView.cellForRow(at: indexPath) as! FormCell
+                cell.textField.text = string
+            }
+            
+        }
+        navigationController?.pushViewController(selectionTVC, animated: true)
+
+        
+    }
+    
+   func selectionIs(string:String, atRow:Int)
+   {
+    let cell = tableView.cellForRow(at: IndexPath(row: atRow, section: 0)) as! FormCell
+    cell.textField.text = string
+
+   }
+    
+    deinit {
+       
+        print("Bye bye FormTableViewController")
+    }
+  
+
+}
